80af36aa0067ee784fe303a0dc71d30ceb494a22
Thais Ferreira Passos <thispassos55@gmail.com>
2024-10-18 00:08:29 -0300

    feat: configurações de ambiente

---
+++ src/app/(tabs)/edit.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Edit(){
+
+  return(
+    <View style={{flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: ""}}> 
+      <Text> Edit </Text>
+    </View>
+  )
+}
